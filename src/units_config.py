c2a7d2f07954391be5f49da38eafbf064b78ebbb
t29mato <tomoya.matou@gmail.com>
2025-05-16 21:12:27 +0900

    added thermoelectric config

--- src/units_config.py
+++ src/units_config.py
@@ -6,7 +6,15 @@ UNIT_CONVERSIONS = {
     "Discharge capacity": "mAh/g",
     "Charge capacity": "mAh/g",
     "Voltage": "V",
-    "Cycle number": "–",
-    "C rate": "–",  # 無次元
+    "Cycle number": "-",
+    "C rate": "-",  # 無次元
     "Time": "h",
+
+    # Thermoelectric materials
+    "Seebeck coefficient": "V/K",
+    "Electrical conductivity": "S/m",
+    "Electrical resistivity": "ohm*m",
+    "Thermal conductivity": "W/(m*K)",
+    "Power factor": "W/(m*K**2)",
+    "ZT": "-",
 }
